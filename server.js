5d8c837e9576c98b8e4b3304dde8cc74bfcba0c8
uniman-app <144687208+uniman-app@users.noreply.github.com>
2023-09-11 13:09:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const app = express()
 app.use(express.json())
 
 const conn = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    uri: process.env.MYSQL_URL,
+    port: process.env.MYSQLPORT
 })
 
 const transporter = nodemailer.createTransport({
